c95f059cd3b7cb33d7fca2c333724495f5527a8f
vattcarter7 <vattcarter@gmail.com>
2020-04-10 14:15:01 +0700

    first init

---
+++ README.md
@@ -0,0 +1,27 @@
+## Routes used for first version:
+### Base API URL - https://airbnb-api.robertbunch.dev
+- /cities/recommended
+- /cities/:feature
+    - Available features:
+        - beach
+        - europe
+        - asia
+        - us
+        - exotic
+- /city/:cityId
+
+- /venues/recommended
+- /venues/superHost
+- /venue/:venueId
+
+- /activities/today
+- /activities/:feature
+    - scenery
+    - baking
+    - diving
+    - animals
+- /activity/:id
+
+- /seach/:searchTerm
+
+- /points/get
---
+++ src/utility/NavBar/NavBar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './NavBar.css';
+import { Link } from 'react-router-dom';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import openModal from '../../actions/openModal';
+import Login from '../../pages/Login/Login';
+import SignUp from '../../pages/Login/SignUp';
+
+class NavBar extends Component{
+
+    render(){
+
+        let navColor = 'transparent';
+        if(this.props.location.pathname !== '/'){
+            // then user is not on the home page!
+            navColor = 'black'
+        }
+    
+        return(<div className="container-fluid nav">
+            <div className="row">
+                <nav className={navColor}>
+                    <div className="nav-wrapper">
+                        <Link to="/" className="left">airbnb</Link>
+                        <ul id="nav-mobile" className="right">
+                            <li><Link to="/">English (US)</Link></li>
+                            <li><Link to="/">$ USD</Link></li>
+                            <li><Link to="/">Become a host</Link></li>
+                            <li><Link to="/">Help</Link></li>
+                            <li onClick={()=>{this.props.openModal('open',<SignUp />)}}>Sign up</li>
+                            <li onClick={()=>{this.props.openModal('open',<Login />)}}>Log in</li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        )
+    }
+}
+
+function mapDispatchToProps(dispatcher){
+    return bindActionCreators({
+        openModal: openModal
+    },dispatcher)
+}
+
+export default connect(null, mapDispatchToProps)(NavBar);
