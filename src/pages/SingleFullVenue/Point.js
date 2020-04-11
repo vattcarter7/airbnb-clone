99648d49eb681920a8b2622b3a9f722c14334227
vattcarter7 <vattcarter@gmail.com>
2020-04-11 15:13:06 +0700

    added logo and fixed bugs for the singlevenue

--- src/pages/SingleFullVenue/Point.js
+++ src/pages/SingleFullVenue/Point.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Point = ({ pointDesc, point }) => {
   const descObj = pointDesc.find((p) => p.pointTitle === point);
+  if (!descObj) return <div></div>;
   return (
     <div>
       <div className='point-title'>{point}</div>
